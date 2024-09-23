f79182803c7b92b4006cce902568003d10048b6c
Niraj Jadhav <75793791+jadhavniraj26@users.noreply.github.com>
2024-09-23 20:05:25 +0530

    Create ArrayAddition.java
    Java Program to Calculate Sum of the Elements of the Array

---
+++ ArrayAddition.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ArrayAddition  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        // Asking for array size
+        System.out.println("Enter the Size of Array: ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        // Taking input for array elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Element " + i + " :");
+            array[i] = sc.nextInt();
+        }
+        
+        // Displaying the array after input is complete
+        System.out.println("The Entered Array:");
+        System.out.println(Arrays.toString(array)); // Display the array elements
+        
+        for (int i = 0; i < n; i++){
+            sum += array[i];
+        }
+        System.out.println("The Sum of the elements of the arrays : "+sum);
+    }
+}
